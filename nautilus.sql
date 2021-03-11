10c661e92612f7abe9a5034e3fd64efc98e02143
pgallegosc <pablo.gallegos@nautilus.team>
2021-03-11 12:58:17 -0300

    update files account

--- nautilus.sql
+++ nautilus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-03-2021 a las 16:50:47
+-- Tiempo de generación: 11-03-2021 a las 16:57:03
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.11
 
@@ -29,12 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `last_name` varchar(200) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `password2` varchar(50) NOT NULL
+  `name` varchar(200) DEFAULT NULL,
+  `last_name` varchar(200) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone_country_code` int(11) DEFAULT NULL,
+  `phone_number` int(11) DEFAULT NULL,
+  `security_code` int(11) DEFAULT NULL,
+  `device_fingerprints` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `last_name`, `user_name`, `email`, `phone_country_code`, `phone_number`, `security_code`, `device_fingerprints`) VALUES
+(2, 'string', 'string', 'string', 'user@example.com', 0, 0, 0, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -54,7 +64,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
